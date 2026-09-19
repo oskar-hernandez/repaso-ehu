a7ceeaca564c199354361c3831789fa5ad96902e
oskar-hernandez <oskar.hernandez@factorial.co>
2026-09-19 16:06:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE = 'repaso-ehu-v1';
-const CORE = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
-
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png'];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(CORE); }).then(function () { return self.skipWaiting(); }));
 });
